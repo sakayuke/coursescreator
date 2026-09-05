84523bc27986974a2a9072cd8fc5e4537375d1fd
Kana-chan <zenakozin860@gmail.com>
2026-09-05 13:50:54 +0200

    Add user profile fields and editing

--- app/models.py
+++ app/models.py
@@ -9,6 +9,16 @@ class User(UserMixin, db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
 
+    first_name = db.Column(
+        db.String(100),
+        nullable=False
+    )
+
+    last_name = db.Column(
+        db.String(100),
+        nullable=False
+    )
+
     email = db.Column(
         db.String(255),
         unique=True,
